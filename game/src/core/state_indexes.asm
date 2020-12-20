4a978b030cd1a3c39994cc94a58f36257bd5da5c
andwhyisit <realmmaster26@hotmail.com>
2020-12-20 21:44:11 +1000

    Transport menu and medawatch partial disasms.

--- game/src/core/state_indexes.asm
+++ game/src/core/state_indexes.asm
@@ -4,6 +4,9 @@ SECTION "State Machine Indexes", WRAM0[$C480]
 W_CoreStateIndex:: ds 1
 W_CoreSubStateIndex:: ds 1
 
+SECTION "Transport Option State Indexes", WRAM0[$C57A]
+W_TransportOptionSubSubSubStateIndex:: ds 1
+
 SECTION "Item Action State Indexes", WRAM0[$C57C]
 W_ItemActionSubSubStateIndex:: ds 1
 W_ItemActionSubSubSubStateIndex:: ds 1
@@ -36,6 +39,12 @@ IncSubStateIndex::
 SECTION "Increment Naming Screen SubSubstate Index", ROMX[$5838], BANK[$01]
 IncNamingScreenSubSubStateIndex::
   ld hl, W_NamingScreenSubSubStateIndex
+  inc [hl]
+  ret
+
+SECTION "Increment Transport Option SubSubSubState Index", ROMX[$4A0D], BANK[$06]
+IncTransportOptionSubSubSubStateIndex::
+  ld hl, W_TransportOptionSubSubSubStateIndex
   inc [hl]
   ret
 
