274df3182fc919ccceecffeacc199b0fe287a38e
VariantXYZ <variantxyz@gmail.com>
2021-05-30 20:48:31 -0700

    Fix font code handling

--- scripts/dialog2bin.py
+++ scripts/dialog2bin.py
@@ -159,8 +159,8 @@ with open(input_file, 'r', encoding='utf-8-sig') as fp:
                         bintext.append(int( special_type + ''.join(special_data), 16))
                     elif special_type == 'f': # Font type
                         s = ''.join(special_data)
-                        result.append(0xD4)
-                        result.append(special_data, 16) # f[00, FF], 0 is normal
+                        bintext.append(0xD4)
+                        bintext.append(int(s, 16)) # f[00, FF], 0 is normal
                     else:
                         raise Exception(f"Unknown special_type {special_type} in {txt}")
             finally:
