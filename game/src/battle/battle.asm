ec6dd7bb59e96ae49687cd9e91771a15d8f2ea9d
VariantXYZ <variantxyz@gmail.com>
2022-06-21 23:40:09 -0700

    Move Medaforce preparation function into battle, it applies to Player and NPCs

--- game/src/battle/battle.asm
+++ game/src/battle/battle.asm
@@ -1,4 +1,5 @@
 INCLUDE "game/src/common/constants.asm"
+INCLUDE "game/src/common/macros.asm"
 
 SECTION "Battle Helper Functions 1", ROMX[$4DCF], BANK[$0A]
 ; Part of initial state (0A:40C1 -> 0A:4B84 -> 0A:4DCF)
@@ -831,3 +832,148 @@ MapPlayerMedachangeAttackCNameForBattle::
   jr nz, .loop
   pop de
   ret
+
+SECTION "'Medaforce' selection", ROMX[$5cff], BANK[$0A]
+BattlePrepareMedaforce::
+  xor a
+  ld [$c4f1], a
+  ld a, [$dc10]
+  cp $28
+  jp z, .no_medaforce_selected
+  ld a, b
+  cp $ff
+  jr z, .check_table
+  ld [$c4f0], a
+  ld a, $01
+  ld [$c4f1], a
+  jr .ignore_table
+.check_table
+  call $ab4
+  ld a, [$c4a0]
+  and $07
+  ld hl, .table
+  ld b, $00
+  ld c, a
+  add hl, bc
+  ld a, [hl]
+  ld [$c4f0], a
+.ignore_table
+  xor a
+  ld [$c4f2], a
+.check_next
+  ld hl, $15
+  ld b, $00
+  ld a, [$c4f0]
+  ld c, a
+  add hl, bc
+  add hl, de
+  ld a, [hl]
+  cp $ff
+  jr z, .no_medaforce
+  ld [W_ListItemIndexForBuffering], a
+  ld b, $0a
+  ld c, $0f
+  ld a, $00
+  ld [W_ListItemInitialOffsetForBuffering], a
+  push de
+  call WrapBufferTextFromList
+  pop de
+  ld a, [$c546]
+  ld b, a
+  ld hl, $1dc
+  add hl, de
+  ld a, [hl]
+  cp b
+  jr c, .no_medaforce
+  call BattlePrepareMedaforceCopyToStructure
+  jr .medaforce_selected
+.no_medaforce
+  ld a, [$c4f1]
+  or a
+  jr nz, .no_medaforce_selected
+  ld a, [$c4f0]
+  sub $01
+  jr nc, .increment
+  ld a, $02
+.increment
+  ld [$c4f0], a
+  ld a, [$c4f2]
+  inc a
+  ld [$c4f2], a
+  cp $03
+  jr nz, .check_next
+.no_medaforce_selected
+  xor a
+  ret
+.medaforce_selected
+  ld a, $01
+  ret
+.table
+  db $00, $01, $02
+  db $00, $01, $02
+  db $00, $01
+
+  padend $5d8d
+
+BattlePrepareMedaforceCopyToStructure::
+  push de
+  ld hl, $1d0
+  add hl, de
+  ld d, h
+  ld e, l
+  ld hl, W_ListItemBufferArea + $6 ; Medaforce name
+  ld bc, $9
+  call memcpy
+  pop de
+  ld a, [W_ListItemBufferArea + $3]
+  ld hl, $1da
+  add hl, de
+  ld [hli], a
+  ld hl, $15
+  ld b, $00
+  ld a, [$c4f0]
+  ld c, a
+  add hl, bc
+  add hl, de
+  ld a, [hl]
+  push af
+  ld hl, $1e0
+  add hl, de
+  pop af
+  ld [hli], a
+  ld hl, $1db
+  add hl, de
+  ld a, [W_ListItemBufferArea]
+  ld [hl], a
+  ld hl, $1dd
+  add hl, de
+  ld a, [W_ListItemBufferArea + $1]
+  ld [hli], a
+  ld hl, $1de
+  add hl, de
+  ld a, [W_ListItemBufferArea + $2]
+  ld [hli], a
+  ld hl, $1e1
+  add hl, de
+  ld a, [W_ListItemBufferArea + $4]
+  ld [hli], a
+  ld hl, $1df
+  add hl, de
+  ld a, [W_ListItemBufferArea + $5]
+  ld [hli], a
+  ld hl, $1e6
+  add hl, de
+  ld a, [$c4f0]
+  inc a
+  ld [hli], a
+  ld hl, $a2
+  add hl, de
+  ld a, [$c4f0]
+  add $04
+  ld [hl], a
+  ld hl, $1e3
+  add hl, de
+  ld [hl], $00
+  ret
+
+  padend $5dfb
