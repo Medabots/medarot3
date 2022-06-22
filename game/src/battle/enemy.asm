509e585292fa8b1e0350b41c88bd1e427c7f04aa
VariantXYZ <variantxyz@gmail.com>
2022-06-21 19:53:23 -0700

    Dump routine to populate enemy Medaforce selection

--- game/src/battle/enemy.asm
+++ game/src/battle/enemy.asm
@@ -209,3 +209,148 @@ BattleLoadEnemyDataMisc::
   ld [$c4f0], a
   jr nz, .next_participant
   ret
+
+SECTION "Enemy 'Medaforce' selection", ROMX[$5cff], BANK[$0A]
+BattlePrepareEnemyMedaforce::
+  xor a
+  ld [$c4f1], a
+  ld a, [$dc10]
+  cp $28
+  jp z, .asm_29d80
+  ld a, b
+  cp $ff
+  jr z, .asm_29d1a
+  ld [$c4f0], a
+  ld a, $01
+  ld [$c4f1], a
+  jr .asm_29d2d
+.asm_29d1a
+  call $ab4
+  ld a, [$c4a0]
+  and $07
+  ld hl, $5d85
+  ld b, $00
+  ld c, a
+  add hl, bc
+  ld a, [hl]
+  ld [$c4f0], a
+.asm_29d2d
+  xor a
+  ld [$c4f2], a
+.asm_29d31
+  ld hl, $15
+  ld b, $00
+  ld a, [$c4f0]
+  ld c, a
+  add hl, bc
+  add hl, de
+  ld a, [hl]
+  cp $ff
+  jr z, .asm_29d63
+  ld [W_ListItemIndexForBuffering], a
+  ld b, $0a
+  ld c, $0f
+  ld a, $00
+  ld [W_ListItemInitialOffsetForBuffering], a
+  push de
+  call WrapBufferTextFromList
+  pop de
+  ld a, [$c546]
+  ld b, a
+  ld hl, $1dc
+  add hl, de
+  ld a, [hl]
+  cp b
+  jr c, .asm_29d63
+  call BattlePrepareEnemyMedaforceCopyToStructure
+  jr .asm_29d82
+.asm_29d63
+  ld a, [$c4f1]
+  or a
+  jr nz, .asm_29d80
+  ld a, [$c4f0]
+  sub $01
+  jr nc, .asm_29d72
+  ld a, $02
+.asm_29d72
+  ld [$c4f0], a
+  ld a, [$c4f2]
+  inc a
+  ld [$c4f2], a
+  cp $03
+  jr nz, .asm_29d31
+.asm_29d80
+  xor a
+  ret
+.asm_29d82
+  ld a, $01
+  ret
+
+  padend $5d85
+
+  db $00, $01, $02
+  db $00, $01, $02
+  db $00, $01
+
+BattlePrepareEnemyMedaforceCopyToStructure::
+  push de
+  ld hl, $1d0
+  add hl, de
+  ld d, h
+  ld e, l
+  ld hl, W_ListItemBufferArea + $6 ; Medaforce name
+  ld bc, $9
+  call memcpy
+  pop de
+  ld a, [W_ListItemBufferArea + $3]
+  ld hl, $1da
+  add hl, de
+  ld [hli], a
+  ld hl, $15
+  ld b, $00
+  ld a, [$c4f0]
+  ld c, a
+  add hl, bc
+  add hl, de
+  ld a, [hl]
+  push af
+  ld hl, $1e0
+  add hl, de
+  pop af
+  ld [hli], a
+  ld hl, $1db
+  add hl, de
+  ld a, [W_ListItemBufferArea]
+  ld [hl], a
+  ld hl, $1dd
+  add hl, de
+  ld a, [W_ListItemBufferArea + $1]
+  ld [hli], a
+  ld hl, $1de
+  add hl, de
+  ld a, [W_ListItemBufferArea + $2]
+  ld [hli], a
+  ld hl, $1e1
+  add hl, de
+  ld a, [W_ListItemBufferArea + $4]
+  ld [hli], a
+  ld hl, $1df
+  add hl, de
+  ld a, [W_ListItemBufferArea + $5]
+  ld [hli], a
+  ld hl, $1e6
+  add hl, de
+  ld a, [$c4f0]
+  inc a
+  ld [hli], a
+  ld hl, $a2
+  add hl, de
+  ld a, [$c4f0]
+  add $04
+  ld [hl], a
+  ld hl, $1e3
+  add hl, de
+  ld [hl], $00
+  ret
+
+  padend $5dfb
