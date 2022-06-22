d839a9e69ab24aab989e70003647719c17161bf6
VariantXYZ <variantxyz@gmail.com>
2022-06-21 18:29:59 -0700

    Dump routines related to loading battle text buffers for Medaforce use

--- game/src/battle/buffer_text_0C.asm
+++ game/src/battle/buffer_text_0C.asm
@@ -39,66 +39,7 @@ BattleLoadTextBuf01::
 
 SECTION "Load text for part damage 0C", ROMX[$5676], BANK[$0C]
 BattleLoadPartDamageNumber:: ;
-  push de
-  xor a
-  ld [$c4fc], a
-  ld de, cBUF00
-  ld a, [$ddb4]
-  ld h, a
-  ld a, [$ddb5]
-  ld l, a
-  ld bc, $64
-  push de
-  call DigitCalculationLoop
-  pop de
-  ld a, [$c4ee]
-  or a
-  jr z, .start_calc
-  add $e0
-  ld [de], a
-  inc de
-  ld a, $01
-  ld [$c4fc], a
-.start_calc
-  ld a, [$c4e0]
-  ld h, a
-  ld a, [$c4e1]
-  ld l, a
-  ld bc, $a
-  push de
-  call DigitCalculationLoop
-  pop de
-  ld a, [$c4ee]
-  or a
-  jr z, .next_digit_1
-  add $e0
-  ld [de], a
-  inc de
-  jr .next_digit_2
-.next_digit_1
-  ld a, [$c4fc]
-  or a
-  jr z, .next_digit_2
-  ld a, $e0
-  ld [de], a
-  inc de
-.next_digit_2
-  ld a, [$c4e0]
-  ld h, a
-  ld a, [$c4e1]
-  ld l, a
-  ld bc, $1
-  push de
-  call DigitCalculationLoop
-  pop de
-  ld a, [$c4ee]
-  add $e0
-  ld [de], a
-  inc de
-  ld a, $cb
-  ld [de], a
-  pop de
-  ret
+  LoadDamageNumberIntoBUF01 0C
 
   LoadPartTypeTextIntoBUF01 0C
   PartTypeTable 0C
