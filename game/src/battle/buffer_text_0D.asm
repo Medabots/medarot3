08a344e5e9d1b03fc1a71465a1156db7b4bd18ca
VariantXYZ <variantxyz@gmail.com>
2022-06-27 14:45:37 -0700

    Dump routine to draw transforming part text

--- game/src/battle/buffer_text_0D.asm
+++ game/src/battle/buffer_text_0D.asm
@@ -45,3 +45,110 @@ SECTION "Load text into buffers for battle messages 2 0D", ROMX[$48b7], BANK[$0D
   LoadParticipantNameIntoBUF02 0D
 
   padend $48c7
+
+SECTION "Load text for part transformations", ROMX[$5a22], BANK[$0D]
+BattleStatusTransformLoadNewPart::
+  ld a, $01
+  ld [$dd40], a
+  call $4b3a
+  ld hl, $d0
+  call $4b9f
+  ld de, $dd43
+  ld bc, $0020
+  call memcpy
+  call $4b3a
+  ld hl, $a2
+  add hl, de
+  ld a, [hl]
+  dec a
+  ld b, $00
+  ld c, a
+  ld hl, $3
+  add hl, de
+  add hl, bc
+  ld a, [hl]
+  ld [$dd42], a
+  ld a, [$dd41]
+  ld [hl], a
+  ld a, [$dd41]
+  ld [W_ListItemIndexForBuffering], a
+  call $4b3a
+  ld hl, $a2
+  add hl, de
+  ld a, [hl]
+  ld b, a
+  ld c, $09
+  ld a, $07
+  ld [W_ListItemInitialOffsetForBuffering], a
+  call WrapBufferTextFromList
+  call $4b3a
+  ld hl, $d0
+  call $4b9f
+  ld d, h
+  ld e, l
+  ld hl, W_ListItemBufferArea
+  ld bc, $9
+  call memcpy
+  ld a, [$dd41]
+  ld [W_ListItemIndexForBuffering], a
+  call $4b3a
+  ld hl, $a2
+  add hl, de
+  ld a, [hl]
+  dec a
+  call $34ff
+  call $4b3a
+  ld hl, $d9
+  call $4b9f
+  ld a, [$c552]
+  ld [hl], a
+  call $4b3a
+  ld hl, $da
+  call $4b9f
+  ld a, [$c553]
+  ld [hl], a
+  call $4b3a
+  ld hl, $dd
+  call $4b9f
+  ld a, [$c556]
+  ld [hl], a
+  call $4b3a
+  ld hl, $de
+  call $4b9f
+  ld a, [$c557]
+  ld [hl], a
+  call $4b3a
+  ld hl, $df
+  call $4b9f
+  ld a, [$c558]
+  ld [hl], a
+  call $4b3a
+  ld hl, $e8
+  call $4b9f
+  ld a, [$c55f]
+  ld [hl], a
+  call $4b3a
+  call $3672
+  call $4b3a
+  call $429f
+  jp $4b35
+
+BattleStatusLoadTransformedPartIntoBuf02:: ; 35aee (d:5aee)
+  call $4b3a
+  ld hl, $a2
+  add hl, de
+  ld a, [hl]
+  dec a
+  call $4741
+  call $4b3a
+  ld hl, $d0
+  call $4b9f
+  ld de, cBUF02
+  ld bc, $9
+  call memcpy
+  ld bc, $86
+  call $48ae
+  call WrapInitiateMainScript
+  jp $4b35
+
+  padend $5b18
