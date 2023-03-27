a768856dd189bea0682f5bd499e3e23e364083d4
VariantXYZ <VariantXYZ@users.noreply.github.com>
2023-03-26 18:08:33 -0700

    Quick fix for cutscene text

--- game/src/text/misc_text.asm
+++ game/src/text/misc_text.asm
@@ -5,36 +5,57 @@ INCLUDE "game/src/common/macros.asm"
 
 SECTION "Draw transformation cutscene text", ROMX[$43C8], BANK[$01]
 TransformationCutsceneDrawText:
+  rla
+  ; Draw line 1
+  push af
+  ld h, $b0
+  ld de, $99c1 ; TODO: We don't need to map here, since the tilemap already handles it
+  call .sub
+  pop af
+
+  ; Draw line 2
+  inc a
+  ld h, $c0
+  ld de, $9a01
+  call .sub
+
+  ret
+.sub
+  push hl
   ld hl, .text_table
   rst $30
-  ld d, h
-  ld e, l
-  ld hl, $8B00
-  ld b, $20
-.loop
-  ld a, [de]
-  call WrapMainScriptDrawNonKanjiCharacter
-  inc de
-  dec b
-  jr nz, .loop
+  ld b, h
+  ld c, l
+  pop hl
+  ld a, $10 ; 16 tiles to draw
+  call VWFDrawStringLeftFullAddress
   ret
+
 .text_table
-  dw .text1
-  dw .text2
-  dw .text3
-  dw .text4
+  dw .text1_line1
+  dw .text1_line2
+  dw .text2_line1
+  dw .text2_line2
+  dw .text3_line1
+  dw .text3_line2
+  dw .text4_line1
+  dw .text4_line2
 ; 16 bytes per line
-.text1
-  db $22, $77, $11, $32, $ee, $ee, $ee, $ee, $ee, $2e, $73, $b9, $b9, $00, $00, $00 ; メダチェーーーーーンジ!!
-  db $2a, $08, $28, $0d, $23, $ee, $7b, $b9, $b9, $00, $00, $00, $00, $00, $00, $00 ; レクリスモード!!
-.text2
-  db $22, $77, $11, $32, $ee, $ee, $ee, $ee, $ee, $2e, $73, $b9, $b9, $00, $00, $00 ; メダチェーーーーーンジ!!
-  db $08, $27, $1c, $13, $30, $23, $ee, $7b, $b9, $b9, $00, $00, $00, $00, $00, $00 ; クラフティモード!!
-.text3
-  db $39, $69, $40, $67, $b9, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00 ; いっけぇ！
-  db $22, $77, $1c, $33, $ee, $0d, $00, $8f, $63, $3d, $39, $91, $b9, $00, $00, $00 ; メダフォース　ぜんかいだ！
-.text4 ; Empty entry
-  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
-  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+.text1_line1
+  db "Medachaaaange!", $CB ; メダチェーーーーーンジ!!
+.text1_line2
+  db "Reckless Mode!", $CB ; レクリスモード!!
+.text2_line1
+  db "Medachaaaange!", $CB ; メダチェーーーーーンジ!!
+.text2_line2
+  db "Crafty Mode!", $CB ; クラフティモード!!
+.text3_line1
+  db "Let's go!", $CB ; メダフォース　ぜんかいだ！
+.text3_line2
+  db "Medaforce is fully recovered!", $CB ; メダフォース　ぜんかいだ！
+.text4_line1 ; Empty entry
+  db $CB
+.text4_line2
+  db $CB
 
   padend $4464
