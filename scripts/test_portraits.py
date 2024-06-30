871ce0ddc50a2393b8c9c66de5bc6c534fabdd64
VariantXYZ <VariantXYZ@users.noreply.github.com>
2024-06-30 13:26:14 -0700

    Add #NOCHECK_PORTRAITS support

--- scripts/test_portraits.py
+++ scripts/test_portraits.py
@@ -23,12 +23,16 @@ with open(input_file, 'r', encoding='utf-8-sig') as fp:
         if index.startswith("UNUSED") or translated_text.startswith("@"):
             continue
 
-        original_portraits = re.findall(r"\<@.+?\>", original_text)
-        translated_portraits = re.findall(r"\<@.+?\>", translated_text)
+        original_portraits = re.findall(r"((?:\<#NOCHECK_PORTRAIT\>)?\<@.+?\>)", original_text)
+        translated_portraits = re.findall(r"((?:\<#NOCHECK_PORTRAIT\>)?\<@.+?\>)", translated_text)
 
-        if original_portraits != translated_portraits:
+        errors = None
+        if len(original_portraits) != len(translated_portraits):
+            # If the length doesn't match, then short-circuit and show the error
+            errors = [f"Portrait count doesn't match (original {len(original_portraits)} != {len(translated_portraits)})"]
+        elif original_portraits != translated_portraits:
+            errors = [f"Mismatched portrait: {p[1]} -> {p[0]}" for p in zip(original_portraits, translated_portraits) if p[0] != p[1] and not p[1].startswith("<#NOCHECK_PORTRAIT>")]
+        
+        if errors is not None and len(errors) > 0:
             print(f"Portraits: {base_filename}:{index}: ", end = '')
-            if len(original_portraits) != len(translated_portraits):
-                print(f"Portrait count doesn't match (original {len(original_portraits)} != {len(translated_portraits)})")
-            else:
-                print(f'Mismatched portraits: {[f"{p[1]} -> {p[0]}" for p in zip(original_portraits, translated_portraits) if p[0] != p[1]]}')
+            print(errors)
